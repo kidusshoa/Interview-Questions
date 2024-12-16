386de9ed693225a15ee24767f0e65d0600595f43
kidusshoa <captainkevin2008@gmail.com>
2024-12-16 23:06:29 +0300

    add number fix

--- addNumbers.js
+++ addNumbers.js
@@ -1,9 +1,19 @@
 nums = [2, 7, 11, 15];
 
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i <= nums.length; i++) {
+//     if (nums[i] + nums[i + 1] === target) {
+//       return [i, i + 1];
+//     }
+//   }
+// };
+
 var twoSum = function (nums, target) {
-  for (let i = 0; i <= nums.length; i++) {
-    if (nums[i] + nums[i + 1] === target) {
-      return [i, i + 1];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
   }
 };
