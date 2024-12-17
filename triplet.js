c16f454a06fbd52208a68fe185c640d3149f43bf
kidusshoa <captainkevin2008@gmail.com>
2024-12-17 16:42:15 +0300

    triplets

---
+++ triplet.js
@@ -0,0 +1,22 @@
+// Write a function returning true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2 given an array of numbers.
+
+nums = [3, 4, 5];
+
+var triplet = function (nums) {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      for (let f = 0; f < 3; f++) {
+        let num1 = nums[i] * nums[i];
+        let num2 = nums[j] * nums[j];
+        sum = num1 + num2;
+
+        if (sum === Math.pow(nums[f], 2)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
+console.log(triplet(nums));
