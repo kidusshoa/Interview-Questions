3c7d5ffecc478941b78a79c5277fa89cbddc453b
kidusshoa <captainkevin2008@gmail.com>
2024-12-17 12:55:08 +0300

    fix add nums

--- addNumbers.js
+++ addNumbers.js
@@ -1,21 +1,27 @@
 nums = [2, 7, 11, 15];
 
-// var twoSum = function (nums, target) {
-//   for (let i = 0; i <= nums.length; i++) {
-//     if (nums[i] + nums[i + 1] === target) {
-//       return [i, i + 1];
-//     }
-//   }
-// };
-
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] + nums[i + 1] === target) {
+      return [i, i + 1];
+    } else {
+      for (let j = 0; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
       }
     }
   }
 };
 
-console.log(twoSum(nums, 9));
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+console.log(twoSum(nums, 13));
