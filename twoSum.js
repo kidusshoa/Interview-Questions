92eb8bbb4bcce39871817341285bc0e6012b8102
Kidus Zekarias <123260863+kidusshoa@users.noreply.github.com>
2025-02-07 23:58:43 +0300

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,7 @@
 // Determine if there are any two integers in the array whose sum equals the provided value, given an array of integers and a value.
 
 nums = [2, 3, 4, 7, 9];
+nums2 = [];
 
 var sum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
