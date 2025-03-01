c42b1e9233d8368be810028b016adb2a155de7aa
kidusshoa <captainkevin2008@gmail.com>
2025-03-01 22:28:54 +0300

    power of two fix

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -2,11 +2,17 @@ var isPowerOfTwo = function (n) {
   let s = n % 2;
   if (n === 1) {
     return true;
-  } else if (s === 0) {
-    return true;
-  } else {
+  } else if (n < 1) {
     return false;
-  }
+  } else if (s === 0) {
+    while (n > 1) {
+      if (n % 2 !== 0) {
+        return false;
+      }
+      n = n / 2;
+    }
+    return true;
+  } else return false;
 };
 
 console.log(isPowerOfTwo(3));
